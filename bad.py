d255e7977a1b955d9950cb4f753e8776d742ab63
unknown <arielk2356@gmail.com>
2026-03-02 14:11:35 +0200

    try bad python format

---
+++ bad.py
@@ -0,0 +1,30 @@
+import os,sys
+from   datetime import   datetime
+
+
+def   calculate ( x,y ):
+  if(x>y):
+     print(  "x is bigger" )
+  else:
+      print("y is bigger")
+  return  x+y
+
+
+class   user:
+ def __init__( self,name,age ):
+      self.name=name
+      self.age =age
+
+ def greet( self ):
+      print( f"Hello,{self.name} you are {self.age}years old" )
+
+
+
+def main():
+    result=calculate(10,5)
+    u=user( "Ariel",25 )
+    u.greet( )
+    print( "Result is:",result )
+
+if __name__=="__main__" :
+     main( )
