f672b6ac97508de967e96b2a41268e28252f56dd
unknown <arielk2356@gmail.com>
2026-03-03 18:47:53 +0200

    ci: re added commitizen

--- bootstrap-dev.sh
+++ bootstrap-dev.sh
@@ -2,3 +2,17 @@
 
 pip install --no-index --find-links=/internal/wheels -r requirements-dev.txt
 pre-commit install
+
+
+# #!/usr/bin/env bash
+
+# echo "Setting up development environment..."
+
+# # ensure pre-commit exists
+# python -m pip install --upgrade pip
+# python -m pip install pre-commit
+
+# # install hooks
+# pre-commit install --install-hooks
+
+# echo "Dev environment ready."
